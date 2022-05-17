7252911a2f76fd6c7e6456e182a7e050b8b5a318
Hari Shekhar <shekahardtu@gmail.com>
2022-05-17 10:40:02 +0530

    removed i18n support

--- next.config.js
+++ next.config.js
@@ -1,22 +1,14 @@
 /** @type {import('next').NextConfig} */
-
-const { i18n } = require("./next-i18next.config");
-
 const nextConfig = {
   reactStrictMode: true,
-  i18n,
-  pageExtensions: ["page.tsx", "tsx", "js"],
-  eslint: {
-    dirs: ["src/"],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: { and: [/\.tsx?$/] },
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/login": { page: "/login" },
+    };
   },
 };
 
